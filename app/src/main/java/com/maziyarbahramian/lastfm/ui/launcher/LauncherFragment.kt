8551943995ed72b4c9455b6adbc9ae299b9a9711
Maziyar <maziyar.bahramian@gmail.com>
2020-05-17 20:36:11 +0430

    setup Search and RecyclerView

--- app/src/main/java/com/maziyarbahramian/lastfm/ui/launcher/LauncherFragment.kt
+++ app/src/main/java/com/maziyarbahramian/lastfm/ui/launcher/LauncherFragment.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
@@ -13,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.maziyarbahramian.lastfm.R
 import com.maziyarbahramian.lastfm.ui.DataStateListener
 import com.maziyarbahramian.lastfm.ui.MainViewModel
@@ -32,10 +32,7 @@ class LauncherFragment : Fragment() {
 
     lateinit var dataStateListener: DataStateListener
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var launcherRecyclerAdapter: LauncherRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,14 +50,22 @@ class LauncherFragment : Fragment() {
         viewModel = activity?.run {
             ViewModelProvider(this).get(MainViewModel::class.java)
         } ?: throw Exception("Invalid Activity")
+        initRecyclerView()
         subscribeObservers()
+    }
+
+    private fun initRecyclerView() {
+        launcher_recyclerview.apply {
+            layoutManager = GridLayoutManager(activity, 2)
+            launcherRecyclerAdapter = LauncherRecyclerAdapter()
+            adapter = launcherRecyclerAdapter
+        }
     }
 
     private fun subscribeObservers() {
 
         viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
 
-            //todo handle loading and messages
             println("DEBUG: DataState: ${dataState}")
             dataStateListener.onDataStateChange(dataState)
 
@@ -76,8 +81,8 @@ class LauncherFragment : Fragment() {
 
         viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
             viewState.artistItems?.let {
-                //todo setting artist to recyclerview
                 println("DEBUG: Setting blog posts to RecyclerView: $it")
+                launcherRecyclerAdapter.submitList(it)
             }
         })
 
---
+++ app/src/main/java/com/maziyarbahramian/lastfm/ui/launcher/LauncherRecyclerAdapter.kt
@@ -0,0 +1,91 @@
+package com.maziyarbahramian.lastfm.ui.launcher
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.*
+import com.bumptech.glide.Glide
+import com.maziyarbahramian.lastfm.R
+import com.maziyarbahramian.lastfm.api.networkResponse.ArtistItem
+import kotlinx.android.synthetic.main.list_item_artist.view.*
+
+class LauncherRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    val diffCallback = object : DiffUtil.ItemCallback<ArtistItem>() {
+        override fun areItemsTheSame(oldItem: ArtistItem, newItem: ArtistItem): Boolean {
+            return oldItem.name.equals(newItem.name)
+        }
+
+        override fun areContentsTheSame(oldItem: ArtistItem, newItem: ArtistItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(
+        ArtistRecyclerChangeCallback(this),
+        AsyncDifferConfig.Builder(diffCallback).build()
+    )
+
+    fun submitList(list: List<ArtistItem?>) {
+        differ.submitList(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ArtistViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(
+                    R.layout.list_item_artist,
+                    parent, false
+                )
+        )
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ArtistViewHolder -> {
+                holder.bind(differ.currentList.get(position))
+            }
+        }
+    }
+
+    internal inner class ArtistRecyclerChangeCallback(
+        private val adapter: LauncherRecyclerAdapter
+    ) : ListUpdateCallback {
+        override fun onChanged(position: Int, count: Int, payload: Any?) {
+            adapter.notifyItemRangeChanged(position, count, payload)
+        }
+
+        override fun onMoved(fromPosition: Int, toPosition: Int) {
+            adapter.notifyDataSetChanged()
+        }
+
+        override fun onInserted(position: Int, count: Int) {
+            adapter.notifyItemRangeChanged(position, count)
+        }
+
+        override fun onRemoved(position: Int, count: Int) {
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    class ArtistViewHolder(
+        itemView: View
+    ) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(artist: ArtistItem) = with(itemView) {
+            itemView.setOnClickListener {
+
+            }
+
+            Glide.with(itemView.context)
+                .load(artist.image?.get(3)?.text)
+                .into(itemView.artist_img)
+
+            itemView.artist_name_tv.text = artist.name
+            itemView.artist_listener_tv.text = artist.listeners
+        }
+
+    }
+}
